a2ae0dff66e4966fb50b58ac8fc9f57238777634
Tan Jun Rong <tanjunrong@gmail.com>
2017-04-21 11:25:27 +0900

    added icon

---
+++ app/src/main/java/beepbeep/learning_mvvm/login_mvp/MvpActivity.kt
@@ -0,0 +1,32 @@
+package beepbeep.learning_mvvm.login
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import beepbeep.learning_mvvm.R
+import com.jakewharton.rxbinding2.view.RxView
+import com.jakewharton.rxbinding2.widget.RxTextView
+import io.reactivex.Observable
+import kotlinx.android.synthetic.main.activity_login.*
+
+class LoginActivity : AppCompatActivity() {
+
+    lateinit var connector: LoginInteractor;
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        connector = LoginInteractor(object : LoginContract.Input {
+            override val name: Observable<String>
+                get() = RxTextView.textChanges(nameEditText).map { it.toString() }
+            override val favoriteAnimal: Observable<String>
+                get() = RxTextView.textChanges(favoriteAnimalEditText).map { it.toString() }
+            override val buttonEvent: Observable<Unit>
+                get() = RxView.clicks(submitButton).map { Unit }
+        })
+
+        connector.outputViewModel.subscribe {
+            displayTextView.text = it.displayString
+        }
+    }
+}
---
+++ app/src/main/java/beepbeep/learning_mvvm/login_mvp/MvpViewModel.kt
@@ -0,0 +1,3 @@
+package beepbeep.learning_mvvm.login
+
+class LoginViewModel(val name: String, val favoriteAnimal: String, val displayString: String)
