e1e8a472afdd35132644efa35e0182593be0af74
Kittinun Vantasin <kittinun.f@gmail.com>
2017-04-25 10:26:17 +0900

    Scaffold ver2

---
+++ app/src/test/java/beepbeep/learning_mvvm/todo_mvpvm/TodoMvpVmTest.kt
@@ -0,0 +1,12 @@
+package beepbeep.learning_mvvm.todo_mvpvm
+
+import org.hamcrest.CoreMatchers.equalTo
+import org.hamcrest.MatcherAssert.assertThat
+import org.junit.Test
+
+class TodoMvpVmTest {
+    @Test
+    fun sample() {
+        assertThat(2 + 3, equalTo(5))
+    }
+}
