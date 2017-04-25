588d099b5884c1904a2be5ef97a18dba9fc144b6
Kittinun Vantasin <kittinun.f@gmail.com>
2017-04-25 12:01:46 +0900

    Add preloaded feature and add to do feature

--- app/src/main/java/beepbeep/learning_mvvm/todo_mvpvm/TodoMvpVmPresenter.kt
+++ app/src/main/java/beepbeep/learning_mvvm/todo_mvpvm/TodoMvpVmPresenter.kt
@@ -1,4 +1,19 @@
 package beepbeep.learning_mvvm.todo_mvpvm
 
-class TodoMvpVmPresenter(val repository: TodoMvpVmRepository) : TodoMvpVmContract.Output {
+import io.reactivex.Observable
+
+class TodoMvpVmPresenter(view: TodoMvpVmContract.Input, repository: TodoMvpVmRepositoryType) : TodoMvpVmContract.Output {
+    lateinit override var viewModels: Observable<TodoMvpVmViewModel>
+
+    override val items: Observable<List<TodoMvpVmListViewModel>> by lazy { viewModels.map { it.items } }
+
+    init {
+        val initialCommands = repository.loadInitialTodo().map { SetTodo(it) }
+        val addCommands = view.addTodos.map { AddTodo(it) }
+
+        viewModels = Observable.mergeArray(initialCommands, addCommands)
+                .scan(TodoMvpVmViewModel()) { vm, command ->
+                    vm.executeCommand(command)
+                }
+    }
 }
--- app/src/test/java/beepbeep/learning_mvvm/todo_mvpvm/TodoMvpVmTest.kt
+++ app/src/test/java/beepbeep/learning_mvvm/todo_mvpvm/TodoMvpVmTest.kt
@@ -1,12 +1,63 @@
 package beepbeep.learning_mvvm.todo_mvpvm
 
+import io.reactivex.Observable
+import io.reactivex.observers.TestObserver
+import io.reactivex.subjects.PublishSubject
 import org.hamcrest.CoreMatchers.equalTo
+import org.hamcrest.CoreMatchers.hasItem
 import org.hamcrest.MatcherAssert.assertThat
 import org.junit.Test
 
 class TodoMvpVmTest {
     @Test
-    fun sample() {
-        assertThat(2 + 3, equalTo(5))
+    fun `when user initially load the screen, user see preloaded tasks`() {
+        val test = TestObserver<TodoMvpVmViewModel>()
+
+        val repo = object : TodoMvpVmRepositoryType {
+            override fun loadInitialTodo(): Observable<List<TodoMvpVmListViewModel>> {
+                return Observable.just(listOf(TodoMvpVmListViewModel(true, "test1")))
+            }
+        }
+
+        val addTodoSubject = PublishSubject.create<String>()
+        val view = object : TodoMvpVmContract.Input {
+            override val addTodos: Observable<String> = addTodoSubject
+        }
+
+        val presenter = TodoMvpVmPresenter(view, repo)
+
+        presenter.viewModels.subscribeWith(test)
+
+        val items = test.values()
+        val vm = items.last()
+        assertThat(vm.items, hasItem(TodoMvpVmListViewModel(true, "test1")))
+    }
+
+    @Test
+    fun `when user add new to do, user must see new task at the end of the list`() {
+        val test = TestObserver<TodoMvpVmViewModel>()
+
+        val repo = object : TodoMvpVmRepositoryType {
+            override fun loadInitialTodo(): Observable<List<TodoMvpVmListViewModel>> {
+                return Observable.empty()
+            }
+        }
+
+        val addTodoSubject = PublishSubject.create<String>()
+        val view = object : TodoMvpVmContract.Input {
+            override val addTodos: Observable<String> = addTodoSubject
+        }
+
+        val presenter = TodoMvpVmPresenter(view, repo)
+
+        presenter.viewModels.subscribeWith(test)
+
+        addTodoSubject.onNext("New TITLE")
+
+        val items = test.values()
+        val oldSize = items[0].items.size
+        val newSize = items[1].items.size
+        assertThat(oldSize, equalTo(0))
+        assertThat(newSize, equalTo(1))
     }
 }
